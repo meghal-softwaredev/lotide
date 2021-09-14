f1313611656bcb2b4553ead91850114f26d828ab
Meghal <meghal.shah210@gmail.com>
2021-09-14 19:38:19 -0400

    Added without.js

---
+++ without.js
@@ -0,0 +1,19 @@
+const assertArraysEqual = function(actual, expected) {
+  if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let withoutArr = source.filter(remove => !itemsToRemove.includes(remove));
+  console.log(withoutArr);
+}
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);

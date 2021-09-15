1aacf59a44f72e9289d1a935533a54b7ea0e4ad0
Meghal <meghal.shah210@gmail.com>
2021-09-15 14:07:12 -0400

    Added without.js

--- without.js
+++ without.js
@@ -5,16 +5,22 @@ const assertArraysEqual = function(actual, expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   }
 };
+const eqArrays = function(actual, expected) {
+  if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 const without = function(source, itemsToRemove) {
   let withoutArr = source.filter(remove => !itemsToRemove.includes(remove));
   return withoutArr;
 }
-let result1 = without([1, 2, 3], [1]);
-assertArraysEqual(result1, [2, 3]);
 
-let result2 = without(["1", "2", "3"], [1, 2, "3"]);
-assertArraysEqual(result2, ["1", "2"]);
+assertArraysEqual(eqArrays(without([1, 2, 3], [1]), [2, 3]),true);
+
+assertArraysEqual(eqArrays(without(["1", "2", "3"], [1, 2, "3"]),["1", "2"]),true);
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); 

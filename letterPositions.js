4d23198bbd23aeaccebe5752cdc3700ea8ea66ee
Meghal <meghal.shah210@gmail.com>
2021-09-15 19:48:55 -0400

    Letter positions of sentence

---
+++ letterPositions.js
@@ -0,0 +1,29 @@
+const assertArraysEqual = function(actual, expected) {
+  if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+const eqArrays = function(actual, expected) {
+  if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+    return false;
+  } else {
+    return true;
+  }
+};
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== ' ') {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("hello").e, [1]);

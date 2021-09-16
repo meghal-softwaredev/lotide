5c8b9a2c1a5af3c3b461574b4773d6832d3fb2df
Meghal <meghal.shah210@gmail.com>
2021-09-15 20:09:06 -0400

    Find key by Value

---
+++ findKeyByValue.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  }
+};
+const findKeyByValue = function(object, value){
+  return Object.keys(object).find(key => object[key] === value);
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
